c289440b7e15dab01ec8a65dbcb3547b5ac1c101
MarySed <marysedarous011@gmail.com>
2020-01-24 16:39:49 +0900

    refactored users list

--- models/users/list.js
+++ models/users/list.js
@@ -1,10 +1,24 @@
 module.exports = (knex, User) => {
-  return () => {
-    return knex("users")
-      .select()
-      .then((users) => {
-        return users.map((user) => new User(user));
-      });
-    //return Promise.resolve([]); // fix me!
+  return async () => {
+    const allUsers = await knex("users").select();
+
+    const mappedUsers = allUsers.map((user) => new User(user));
+    return mappedUsers;
+
+    //   const allUsersFinal = Promise.all(
+    //     allUsers.map(async (user) => {
+    //       return new User(user);
+    //     })
+    //   );
+    //   return allUsersFinal;
   };
+
+  // return () => {
+  //   return knex("users")
+  //     .select()
+  //     .then((users) => {
+  //       return users.map((user) => new User(user));
+  //     });
+  //   //return Promise.resolve([]); // fix me!
+  // };
 };
